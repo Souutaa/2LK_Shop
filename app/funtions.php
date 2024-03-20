d8525a625df00994ec2a6a52b4eef6592eebc94d
vodinhluan <luandinhvo2002@gmail.com>
2024-03-20 23:36:14 +0700

    Brand/Category/User Controller

---
+++ app/funtions.php
@@ -0,0 +1,56 @@
+<?php
+use App\Models\User;
+use Symfony\Component\Routing\RouteCollection;
+
+function view($name, $model='') {
+    require_once APP_ROOT . '/views/layout.view.php';
+}
+
+function redirect($url) {
+    header("Location: $url");
+    die();
+}
+
+function getPath(RouteCollection $routes, $name) {
+    return $routes->get($name)->getPath();
+}
+
+function connect($source = DB)
+{
+    try {
+        $dbConnection = new PDO($source, DB_USER, DB_PASS);
+        $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    } catch (PDOException $e) {
+        return null;
+    }
+}
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+function isLoggedIn() {
+    if (!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] == false)
+        return false;
+    return true;
+}
+
+function permissionCheck() {
+    
+}
+
+function startSession() {
+    session_start();
+    $_SESSION['showNav'] = true;
+}
+
+function getSessionUser() {
+    $user = new User();
+    $user = unserialize($_SESSION['user']);
+    return $user;
+}
