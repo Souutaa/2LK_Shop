1b269a44ae6a2d930f3209f767e21c4bd2aec462
Bui Tuan Kiet <kietbui2707@gmail.com>
2024-04-18 18:55:17 +0700

    create register view

--- views/layout.view.php
+++ views/layout.view.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Models\User;
 
 if (!isLoggedIn()) {
@@ -26,11 +27,8 @@ if (isLoggedIn()) {
   </title>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;700;800&display=swap"
-    rel="stylesheet" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;700;800&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://unpkg.com/nouislider@10.0.0/distribute/nouislider.min.css" />
   <link rel="stylesheet" href="/2LK_Shop/public/css/bootstrap.min.css">
   <link rel="stylesheet" href="/2LK_Shop/public/css/simple-notify.min.css" />
@@ -53,52 +51,51 @@ if (isLoggedIn()) {
           </a>
         </div>
         <form class="search__form">
-          <?php if ($_SESSION['showNav'] == true): ?>
-                    <input type="text" class="fontAwesome search__input" placeholder="&#xf002;   Search" />
+          <?php if ($_SESSION['showNav'] == true) : ?>
+            <input type="text" class="fontAwesome search__input" placeholder="&#xf002;   Search" />
           <?php endif ?>
         </form>
         <nav class="nav-user">
-          <?php if ($_SESSION['isLoggedIn'] == true): ?>
-                    <a class="nav-user__icon-box" href="<?php echo getPath($routes, 'viewCart') ?>">
-                      <svg class="icon">
-                        <use xlink:href="/2LK_Shop/public/images/SVG/symbol-defs.svg#icon-location-shopping"></use>
-                      </svg>
-                    </a>
-                    <div class="nav-user__user">
-                      <img src="/2LK_Shop/public/images/productImg/Image.png" alt="User photo" class="nav-user__user-photo" />
-                      <ul class="nav-user__dropdown">
-                        <li class="nav-user__user-info">
-                          <h3 class="nav-user__user-name font-size-4 text-color--1">
-                            <?php echo $user->getUsername(); ?>
-                          </h3>
-                          <span class="nav-user__user-email font-size-3 text-color--4">
-                            <?php echo $user->getEmail(); ?>
-                          </span>
-                        </li>
-                        <li class="nav-user__options">
-                          <a href="<?php echo $routes->get('viewOrders')->getPath() ?>" class="nav-user__option font-size-2">Đơn
-                            hàng</a>
-                          <a href="<?php echo $routes->get('viewPersonalInfo')->getPath() ?>"
-                            class="nav-user__option font-size-2">Thông tin cá nhân</a>
-                        </li>
-                        <li class="nav-user__log-out">
-                          <a href="#" class="log-out btn" id="log-out-btn">
-                            <i class="fa-solid fa-right-from-bracket color--red font-size-1"></i>
-                            <span class="color--red font-size-2">Sign Out</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </nav>
-        <?php endif ?>
-        <?php if ($_SESSION['isLoggedIn'] == false): ?>
-                  <a href="/2LK_Shop/login" class="btn btn__secondary">Đăng nhập</a>
-                  <a href="/2LK_Shop/register" class="btn btn__secondary btn__secondary--active">Đăng kí</a>
-        <?php endif ?>
+          <?php if ($_SESSION['isLoggedIn'] == true) : ?>
+            <a class="nav-user__icon-box" href="<?php echo getPath($routes, 'viewCart') ?>">
+              <svg class="icon">
+                <use xlink:href="/2LK_Shop/public/images/SVG/symbol-defs.svg#icon-location-shopping"></use>
+              </svg>
+            </a>
+            <div class="nav-user__user">
+              <img src="/2LK_Shop/public/images/productImg/Image.png" alt="User photo" class="nav-user__user-photo" />
+              <ul class="nav-user__dropdown">
+                <li class="nav-user__user-info">
+                  <h3 class="nav-user__user-name font-size-4 text-color--1">
+                    <?php echo $user->getUsername(); ?>
+                  </h3>
+                  <span class="nav-user__user-email font-size-3 text-color--4">
+                    <?php echo $user->getEmail(); ?>
+                  </span>
+                </li>
+                <li class="nav-user__options">
+                  <a href="<?php echo $routes->get('viewOrders')->getPath() ?>" class="nav-user__option font-size-2">Đơn
+                    hàng</a>
+                  <a href="<?php echo $routes->get('viewPersonalInfo')->getPath() ?>" class="nav-user__option font-size-2">Thông tin cá nhân</a>
+                </li>
+                <li class="nav-user__log-out">
+                  <a href="#" class="log-out btn" id="log-out-btn">
+                    <i class="fa-solid fa-right-from-bracket color--red font-size-1"></i>
+                    <span class="color--red font-size-2">Sign Out</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
         </nav>
+      <?php endif ?>
+      <?php if ($_SESSION['isLoggedIn'] == false) : ?>
+        <a href="/2LK_Shop/login" class="btn btn__secondary">Đăng nhập</a>
+        <a href="/2LK_Shop/register" class="btn btn__secondary">Đăng kí</a>
+      <?php endif ?>
+      </nav>
       </div>
     </header>
-    <?php require ("$name.view.php"); ?>
+    <?php require("$name.view.php"); ?>
     <footer class="footer">
       <div class="footer__container">
         <div class="footer__left">
@@ -157,15 +154,15 @@ if (isLoggedIn()) {
 </body>
 <script src="/2LK_Shop/public/js/index.js"></script>
 <script>
-  $(document).ready(function (e) {
-    $('.search__form').submit(function (e) {
+  $(document).ready(function(e) {
+    $('.search__form').submit(function(e) {
       e.preventDefault();
       let searchString = $('.search__input').val()
       let url = "<?php echo getPath($routes, 'search') ?>"
       window.location.replace(url.replace('{searchStr}', `${searchString}`));
     })
 
-    $('#log-out-btn').click(function (e) {
+    $('#log-out-btn').click(function(e) {
       localStorage.removeItem('cart');
       location.href = '<?php echo $routes->get('logout')->getPath() ?>'
     })
